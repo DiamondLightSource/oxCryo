ddf26166f0121bc08dc8355b6ec30cb824edbd44
Ollie Copping <oliver.copping@diamond.ac.uk>
2026-08-25 09:18:57 +0100

    Rename everything to oxCryo (to match renaming of DLS repo)

--- oxCryoApp/src/oxCryoMain.cpp
+++ oxCryoApp/src/oxCryoMain.cpp
@@ -1,4 +1,4 @@
-/* OXCryoMain.cpp */
+/* oxCryoMain.cpp */
 /* Author:  Marty Kraimer Date:    17MAR2000 */
 
 #include <stddef.h>
@@ -11,13 +11,14 @@
 #include "epicsThread.h"
 #include "iocsh.h"
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
-    if(argc>=2) {    
+    if (argc >= 2)
+    {
         iocsh(argv[1]);
         epicsThreadSleep(.2);
     }
     iocsh(NULL);
     epicsExit(0);
-    return(0);
+    return (0);
 }
